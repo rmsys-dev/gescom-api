a35b7135cf106485ff5cb25386fbed5b0ad0984b
RONESSAN <rones@rmsys.net>
2026-06-23 18:17:24 -0300

    refactor(database): Remover tipos de sequência e índices da tabela de sequências; adicionar coluna de custo anterior à tabela de preços.

--- drizzle/0011_wide_the_phantom.sql
+++ drizzle/0011_wide_the_phantom.sql
@@ -1,7 +1,2 @@
-CREATE TYPE "public"."sequence_type" AS ENUM('VENDA', 'NFE', 'NFSE', 'NFCE', 'MDFE', 'CTE');--> statement-breakpoint
-DROP INDEX "enterprises_sequences_enterprise_idx";--> statement-breakpoint
-ALTER TABLE "enterprises_sequences" ALTER COLUMN "sequence" SET DATA TYPE integer;--> statement-breakpoint
-ALTER TABLE "enterprises_sequences" ADD COLUMN "type" "sequence_type" NOT NULL;--> statement-breakpoint
 ALTER TABLE "prices" ADD COLUMN "actual_real_cost" numeric(14, 4);--> statement-breakpoint
 ALTER TABLE "prices" ADD COLUMN "previous_cost" numeric(14, 4);--> statement-breakpoint
-CREATE UNIQUE INDEX "enterprises_sequences_enterprise_type_uidx" ON "enterprises_sequences" USING btree ("enterprise_id","type");

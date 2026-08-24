b273409ee4863f74a4f7efb7acfc784664a70f49
LeomirDias <leomirdiasti@outlook.com>
2026-08-24 14:55:00 -0300

    ref(sequences): remove arquivo de tabela de sequências, consolidando a lógica no arquivo de empresas

--- src/db/entities/sequences.ts
+++
@@ -1,7 +0,0 @@
-import { sql } from "drizzle-orm";
-import { integer, pgTable, uniqueIndex, uuid } from "drizzle-orm/pg-core";
-import { enterprises } from "./enterprises.js";
-import { tz } from "../functions.js";
-import { sequenceTypeEnum } from "../enums.js";
-
-//Tabela de sequências de empresas
